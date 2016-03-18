630d95bf143579628912e2c64ca2fb38d4b07733
willemolding <willemolding@gmail.com>
2016-03-18 12:58:47 +1100

    basics of algorithm implemented for binary case

--- logistic_python/tests/test_logistic_regression.py
+++ logistic_python/tests/test_logistic_regression.py
@@ -1,4 +1,40 @@
-from logistic_python import LogisticRegression
+import numpy as np
+
+from logistic_python import BinaryLogisticRegression
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+
 
 def test_constructor():
-	clf = LogisticRegression()
+	clf = BinaryLogisticRegression()
+
+def test_classification():
+	# Tests if it can make a classification without failing
+	X, y = make_classification(n_samples=20, n_features=5, random_state=0)
+
+	clf = BinaryLogisticRegression()
+	clf.fit(X, y)
+	probs = clf.predict_proba(X)
+	y_pred = clf.predict(X)
+
+
+def test_compare_sklearn():
+	# Tests to see if the same predictor labels are obtained from sklearn
+	X, y = make_classification(n_samples=10, n_features=5, random_state=0)
+
+	clf = BinaryLogisticRegression()
+	clf.fit(X, y)
+	probs = clf.predict_proba(X)
+	y_pred = clf.predict(X)
+
+	skclf = LogisticRegression(C=1., solver='lbfgs')
+	skclf.fit(X, y)
+	sk_probs = skclf.predict_proba(X)
+	sk_y_pred = skclf.predict(X)
+
+	print probs
+	print sk_probs
+
+	assert(np.all(y_pred == sk_y_pred))
+	assert(np.all(probs == sk_probs))
+
--- logistic_regression.py
+++ logistic_regression.py
@@ -3,14 +3,58 @@ A logistic regression classifier that uses a similar interface to scikit learn.
 """
 
 import numpy as np
+from scipy.optimize import fmin_l_bfgs_b
 
-class LogisticRegression:
+class BinaryLogisticRegression:
 
-	def __init__(self):
-		pass
+    def __init__(self, C=1.):
+        self.C = C
 
-	def fit(self, X, y):
-		pass
+    def fit(self, X, y):
+        # check inputs here
 
-	def predict(self, X):
-		pass
+        self._beta = np.zeros((X.shape[1] + 1, 1))
+        result = fmin_l_bfgs_b(_objective, self._beta, args=(X, y, self.C))
+
+        self._beta = result[0]
+        return self
+
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X), axis=1)
+
+    def predict_proba(self, X):
+        # add the constant
+        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        XBeta = np.dot(X, self._beta).reshape((-1,1))
+
+        probs = 1./(1. + np.exp(-XBeta))
+        return np.hstack((1 - probs, probs))
+
+
+
+
+def _objective(beta, X, y, C):
+    """
+    The objective function. Returns the regularized negative log-likelihood and gradient.
+    C is the inverse regularization parameter
+    """
+
+    # add a constant column to X to simplify the intercept
+    X = np.hstack((np.ones((X.shape[0], 1)), X))
+    # make y a column vector for array broadcasting reasons
+    y = y.reshape((-1,1))
+
+    # precomptue the features multiplied by the coefficients
+    XBeta = np.dot(X, beta).reshape((-1,1))
+
+    # also precompute the exponents as they are used several times
+    exp_XBeta = np.exp(XBeta)
+
+    # the negative log-likelihood
+    neg_ll = C*np.sum(np.log(1. + exp_XBeta) - y*XBeta, axis=0) + 0.5*np.inner(beta, beta)
+
+    # the gradient of the negative log-likelihood
+    grad_neg_ll = C*np.sum((1./ (1. + exp_XBeta))*exp_XBeta*X - y*X, axis=0) + beta
+
+    return neg_ll, grad_neg_ll
+
